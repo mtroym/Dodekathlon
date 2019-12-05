4e92b0b318db0f6d5c9b548228e0aec980b2eccf
AtroUz-.Troy <30402475+mtroym@users.noreply.github.com>
2019-12-05 21:12:33 +0800

    hydra v0.1.0.1     - add some scripts. GL&HF

--- models/helpers.py
+++ models/helpers.py
@@ -1,11 +1,15 @@
 import functools
 import math
+import random
 
+import torch
 import torch.nn as nn
+from torch.autograd import Variable
 from torch.nn import init
-
-
 # added
+from torch.optim import lr_scheduler
+
+
 def weights_init_ada(init_type='gaussian'):
     def init_fun(m):
         classname = m.__class__.__name__
@@ -115,3 +119,46 @@ def get_norm_layer(norm_type='instance'):
     else:
         raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
     return norm_layer
+
+
+def get_scheduler(optimizer, opt):
+    if opt.lr_policy == 'lambda':
+        scheduler = lr_scheduler.LambdaLR(optimizer,
+                                          lr_lambda=lambda epoch: 1.0 - max(0, epoch + 1 + opt.epoch_count - opt.niter) / float(opt.niter_decay + 1))
+    elif opt.lr_policy == 'step':
+        scheduler = lr_scheduler.StepLR(optimizer, step_size=opt.lr_decay_iters, gamma=0.1)
+    elif opt.lr_policy == 'plateau':
+        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
+    else:
+        return NotImplementedError('learning rate policy [%s] is not implemented', opt.lr_policy)
+    return scheduler
+
+
+class ImagePool:
+    def __init__(self, pool_size):
+        self.pool_size = pool_size
+        if self.pool_size > 0:
+            self.num_imgs = 0
+            self.images = []
+
+    def query(self, images):
+        if self.pool_size == 0:
+            return Variable(images)
+        return_images = []
+        for image in images:
+            image = torch.unsqueeze(image, 0)
+            if self.num_imgs < self.pool_size:
+                self.num_imgs = self.num_imgs + 1
+                self.images.append(image)
+                return_images.append(image)
+            else:
+                p = random.uniform(0, 1)
+                if p > 0.5:
+                    random_id = random.randint(0, self.pool_size - 1)
+                    tmp = self.images[random_id].clone()
+                    self.images[random_id] = image
+                    return_images.append(tmp)
+                else:
+                    return_images.append(image)
+        return_images = Variable(torch.cat(return_images, 0))
+        return return_images
